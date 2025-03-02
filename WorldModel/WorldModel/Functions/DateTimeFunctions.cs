69125c78dd25529b73e00d5c27cc9bffa490abeb
K.V. <30656341+KVonGit@users.noreply.github.com>
2025-03-02 03:37:22 -0600

    Restore Pertex's functions

--- WorldModel/WorldModel/Functions/DateTimeFunctions.cs
+++ WorldModel/WorldModel/Functions/DateTimeFunctions.cs
@@ -11,6 +11,30 @@ namespace TextAdventures.Quest.Functions
         {
             return (int)(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
         }
+		
+        public static string CurrentDate()
+        {
+            DateTime localDate = DateTime.Now; 
+            return localDate.ToShortDateString();
+        }
+
+        public static string CurrentTime()
+        {
+            DateTime localTime = DateTime.Now;
+            return localTime.ToShortTimeString();
+        }
+        
+        public static string CurrentUtcDate()
+        {
+            DateTime utcDate = DateTime.UtcNow; 
+            return utcDate.ToShortDateString();
+        }
+
+        public static string CurrentUtcTime()
+        {
+            DateTime utcTime = DateTime.UtcNow;
+            return utcTime.ToShortTimeString();
+        }
 
     }
 }
